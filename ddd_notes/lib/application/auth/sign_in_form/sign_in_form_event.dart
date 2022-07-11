96ebf5396daa436bf080d0758aa9ef5b28437139
Dibyam <dibyam1101@gmail.com>
2022-07-11 13:27:09 +0530

    Added Auth Failures and Sign In Bloc

---
+++ ddd_notes/lib/application/auth/sign_in_form/sign_in_form_event.dart
@@ -0,0 +1,11 @@
+part of 'sign_in_form_bloc.dart';
+
+@freezed
+abstract class SignInFormEvent with _$SignInFormEvent {
+  const factory SignInFormEvent.emailChanged(String emailStr) = EmailChanged;
+  const factory SignInFormEvent.passwordChanged(String passwordStr) = PasswordChanged;
+  const factory SignInFormEvent.registerWithEmailAndPasswordPressed(String emailStr) = RegisterWithEmailAndPasswordPressed;
+  const factory SignInFormEvent.signInWithEmailAndPasswordPressed(String emailStr) = SignInWithEmailAndPasswordPressed;
+  const factory SignInFormEvent.signInWithGooglePressed(String emailStr) = SignInWithGooglePressed;
+
+}
---
+++ ddd_notes/lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_notes/domain/auth/auth_failure.dart';
+import 'package:ddd_notes/domain/auth/value_objects.dart';
+
+//FirebaseAuth, GoogleSignIn
+//Facade helps combine FirebaseAuth and GoogleSignIn Classes into one single interface
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<void> signInWithGoogle();
+}
